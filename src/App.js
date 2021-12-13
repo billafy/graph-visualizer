0196841c4596ed1946b9c7e6361579ff78727e60
Yash Tarkar <ytarkar138@gmail.com>
2021-12-14 00:57:04 +0530

    'fs-fix'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import React from 'react'
+import React from "react";
 
-// import Header from './components/Header';
-// import HandwritingGenerator from './components/HandwritingGenerator'
-// import NotSupported from './components/NotSupported';
-
-/* import SortingVisualizer from './extras/SortingVisualizer'; */
-
-// import AppProvider from './context';
-
-import Graph from './extras/Graph';
+import Graph from "./Graph";
 
 const App = () => {
-	return (
-		<Graph/>
-		/* <AppProvider>
-			<Header/>
-			<HandwritingGenerator/>
-			<NotSupported/>
-		</AppProvider> */
-		// <SortingVisualizer/>
-	)
-}
+	return <Graph />;
+};
 
 export default App;
