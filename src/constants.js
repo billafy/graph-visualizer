31198bc27ed655946901d844c1cbf7662aa037c4
Yash Tarkar <ytarkar138@gmail.com>
2021-12-19 00:21:12 +0530

    'style-tweaks'

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const moves = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-export const searchSpeed = 1;
-export const backtrackSpeed = 100;
+export const searchSpeed = 10;
+export const backtrackSpeed = 75;
 export const cellSize = 40;
 export const graphHeight = 680;
