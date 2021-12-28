53131f2ce530863223cf974207e69791a4c05892
Yash Tarkar <ytarkar138@gmail.com>
2021-12-28 18:40:06 +0530

    'speedfix'

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const moves = [
 	[-1, 0],
 	[0, -1],
 ];
-export const searchSpeed = 10;
-export const backtrackSpeed = 25;
+export const searchSpeed = 12;
+export const backtrackSpeed = 30;
 export const cellSize = 40;
 export const graphHeight = 680;
